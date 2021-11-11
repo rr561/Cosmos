12590cc939e47f8e64b39141f2c1304e7f16954c
rr561 <ronron_21@yahoo.com>
2021-11-11 11:28:50 +0200

    CosmosCommit

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+   $('.listContainer').slick({
+      prevArrow:'<button class="prevBtn"><span>‹</span></button>',
+      nextArrow:'<button class="nextBtn"><span>›</span></button>',
+      swipe:true,
+      speed:1000,
+      slidesToShow: 1,
+    });
+  });
+
